ca2e2920b1c36fa668402d8b62256b537203a418
Jaka Hatlak <hatlakjaka@gmail.com>
2025-01-06 05:53:01 +0100

    Updated

--- quasi_random_events.py
+++ quasi_random_events.py
@@ -1,15 +1,38 @@
 import random
 import numpy as np
-import time
 
+
+# import time
 # random.seed(time.time())
 
 trial_No = 20
+time_delay = [1, 3]
 
 ones = np.ones(int(trial_No/2))
 zeros = np.zeros(int(trial_No/2))
 
 events = np.append(ones, zeros)
 random.shuffle(events)
-print(events)
 
+
+for trial in events:
+    if trial == 1:
+        print("Trial: UP")
+        while not input() == "q":
+            pass
+        else:
+            print("Target Reached")
+            while not input() == "a":
+                pass
+            else:
+                print("Next Trial")
+    else:
+        print("Trial: DOWN")
+        while not input() == "q":
+            pass
+        else:
+            print("Target Reached")
+            while not input() == "a":
+                pass
+            else:
+                print("Next Trial")
